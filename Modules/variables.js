b0701c102213f16da78bbf0474cf06526212cf96
samuel <samif1@gmail.com>
2023-04-04 01:36:35 +0300

    Fix linter errors

--- Modules/variables.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-mutable-exports
-
-export const bookContainer = document.querySelector('.books-container');
-export const bookTitle = document.querySelector('#title');
-export const bookAuthor = document.querySelector('#author');
-export const addBtn = document.querySelector('.add-btn');
--- index.js
+++ index.js
@@ -1,42 +1,62 @@
-/* eslint-disable import/no-unresolved */
-import { DateTime } from './modules/luxon.js';
-import { displayBooks, addNew, showContact } from './modules/addEvent.js';
-import {
-  pushToLocal,
-  showBook,
-  addBook,
-  bookUpload,
-} from './modules/add&Remove.js';
+import Book from './modules/book.js';
+import library from './modules/library.js';
+import UI from './modules/UI.js';
+import { DateTime } from './modules/time.js';
 
-window.onload = () => {
-  showBook();
-};
-
-const addBtn = document.querySelector('.add-btn');
-
-addBtn.addEventListener('click', (e) => {
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+document.querySelector('.book-form').addEventListener('submit', (e) => {
   e.preventDefault();
-  addBook();
-  bookUpload();
-  pushToLocal();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const book = new Book(title, author);
+  UI.addBook(book);
+  library.addBook(book);
+  UI.clrFields();
 });
 
-const showDateAndTime = document.getElementById('date');
-const now = DateTime.now();
-showDateAndTime.innerHTML = now.toLocaleString(DateTime.DATETIME_MED);
-
-const showBooks = document.querySelector('.Book-ListBtn');
-const addNewBook = document.querySelector('.AddBook-Btn');
-const displayContacts = document.querySelector('.contact');
-
-showBooks.addEventListener('click', () => {
-  displayBooks();
+document.querySelector('.table-body').addEventListener('click', (e) => {
+  UI.delBook(e.target);
+  library.remBk(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
 });
 
-addNewBook.addEventListener('click', () => {
-  addNew();
+const curDate = document.querySelector('.dateTime');
+
+const dt = DateTime.local();
+const newDate = dt.toLocaleString({
+  month: 'long',
+  day: 'numeric',
+  year: 'numeric',
 });
 
-displayContacts.addEventListener('click', () => {
-  showContact();
+const newTime = dt.toLocaleString(DateTime.TIME_WITH_SECONDS);
+curDate.innerHTML = `
+    <p>${newDate} &nbsp ${newTime}</p>
+  `;
+
+const logo = document.querySelector('.logo');
+const booklist = document.querySelector('.list-one');
+const add = document.querySelector('.list-two');
+const contact = document.querySelector('.list-three');
+const renderBooks = document.querySelector('.books');
+const forms = document.querySelector('.forms');
+const contacts = document.querySelector('.contact');
+logo.addEventListener('click', () => {
+  renderBooks.classList.remove('active');
+  forms.classList.remove('active');
+  contacts.classList.remove('active');
+});
+booklist.addEventListener('click', () => {
+  renderBooks.classList.remove('active');
+  forms.classList.remove('active');
+  contacts.classList.remove('active');
+});
+add.addEventListener('click', () => {
+  renderBooks.classList.add('active');
+  forms.classList.add('active');
+  contacts.classList.remove('active');
+});
+contact.addEventListener('click', () => {
+  renderBooks.classList.add('active');
+  forms.classList.remove('active');
+  contacts.classList.add('active');
 });
