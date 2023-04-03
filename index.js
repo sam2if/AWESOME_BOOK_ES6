d59ff71c8cfdfd233357858d5ab1acaf1f2ad0bb
samuel <samif1@gmail.com>
2023-04-04 00:53:23 +0300

    Finishing everything

--- index.js
+++ index.js
@@ -1,103 +1,42 @@
-const button = document.querySelector('#button');
-const store = document.querySelector('#myList');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const date = new Date();
-const now = new Date().toLocaleDateString('en-us', { month: 'long'});
-const currentTime = `${now} ${date.getDate()} th ${date.getFullYear()} , ${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
-document.getElementById('demo').innerHTML = currentTime;
+/* eslint-disable import/no-unresolved */
+import { DateTime } from './modules/luxon.js';
+import { displayBooks, addNew, showContact } from './modules/addEvent.js';
+import {
+  pushToLocal,
+  showBook,
+  addBook,
+  bookUpload,
+} from './modules/add&Remove.js';
 
-let Book = [];
+window.onload = () => {
+  showBook();
+};
 
-function Booklist(title, author, id) {
-  this.title = title.value;
-  this.author = author.value;
-  this.id = Book.length;
-}
+const addBtn = document.querySelector('.add-btn');
 
-function remove(b) {
-  const filt = Book.filter((a, i) => {
-    if (b === a.id) {
-      Book.splice(i, 1);
-      const rem = document.getElementById(`m${b}`);
-      rem.remove();
-    }
-  });
-  localStorage.setItem('bookList', JSON.stringify(Book));
-}
-
-function show() {
-  let id;
-  const book = new Booklist(title, author, id);
-  Book.push(book);
-  const node = document.createElement('li');
-  const len = Book.length;
-  for (let i = 0; i < len; i += 1) {
-    node.setAttribute('id', `m${i}`);
-    node.innerHTML = `<div class="title"><h5>"${Book[i].title}" by ${Book[i].author} </h5></div> <button onclick=remove(${Book[i].id})>Remove</button>`;
-    store.appendChild(node);
-  }
-  title.value = '';
-  author.value = '';
-  localStorage.setItem('bookList', JSON.stringify(Book));
-}
-
-function updateBookList() {
-  store.innerHTML = '';
-  const len = Book.length;
-  for (let i = 0; i < len; i += 1) {
-    const node = document.createElement('li');
-    node.setAttribute('id', `m${i}`);
-    node.innerHTML = `<div class="title"><h5>"${Book[i].title}" by ${Book[i].author} </h5></div><button class="remove"onclick=remove(${Book[i].id})>Remove</button>`;
-    store.appendChild(node);
-  }
-}
-
-if (localStorage.getItem('bookList')) {
-  Book = JSON.parse(localStorage.getItem('bookList'));
-  updateBookList();
-}
-
-button.addEventListener('click', show);
-
-const cont = document.querySelector('.cont');
-const list = document.querySelector('.list');
-const addnew = document.querySelector('.addnew');
-const contact = document.querySelector('.contacts');
-const forms = document.querySelector('.forms');
-const lll = document.querySelector('.lll');
-cont.style.display = 'none';
-
-list.addEventListener('click', () => {
-  forms.style.display = 'none';
-  cont.style.display = 'none';
-  store.style.display = 'block';
-  store.style.border = '2px solid black';
-  lll.textContent = ' All awesome books';
-  list.style.color = 'rgb(82, 82, 234)';
-  contact.style.color = 'black';
-  addnew.style.color = 'black';
-  updateBookList();
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBook();
+  bookUpload();
+  pushToLocal();
 });
 
-addnew.addEventListener('click', () => {
-  store.style.display = 'none';
-  cont.style.display = 'none';
-  forms.style.display = 'flex';
-  addnew.style.color = 'rgb(82, 82, 234)';
-  list.style.color = 'black';
-  contact.style.color = 'black';
-  lll.textContent = 'Add a new book';
-  updateBookList();
+const showDateAndTime = document.getElementById('date');
+const now = DateTime.now();
+showDateAndTime.innerHTML = now.toLocaleString(DateTime.DATETIME_MED);
+
+const showBooks = document.querySelector('.Book-ListBtn');
+const addNewBook = document.querySelector('.AddBook-Btn');
+const displayContacts = document.querySelector('.contact');
+
+showBooks.addEventListener('click', () => {
+  displayBooks();
 });
 
-contact.addEventListener('click', () => {
-  store.style.display = 'none';
-  cont.style.display = 'block';
-  forms.style.display = 'none';
-  contact.style.color = 'rgb(82, 82, 234)';
-  addnew.style.color = 'black';
-  list.style.color = 'black';
-  store.style.border = 'none';
-  lll.textContent = '';
+addNewBook.addEventListener('click', () => {
+  addNew();
+});
+
+displayContacts.addEventListener('click', () => {
+  showContact();
 });
